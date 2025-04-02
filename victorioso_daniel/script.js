696a788816d0d89053ab46b5b99677277565a09a
victorioso_daniel <danielvictorioso.acd@gmail.com>
2025-04-02 22:27:08 +0800

    fix spaces

--- victorioso_daniel/script.js
+++ victorioso_daniel/script.js
@@ -6,9 +6,8 @@ const sortSelection = document.getElementById("sort_selection");
 
 disableCommentButton();
 
-const comments = Array.from(document.querySelectorAll(".comment-box")).map(
-	(comment) => initializeComments(comment)
-);
+const comments = Array.from(document.querySelectorAll(".comment-box"))
+	.map((comment) => initializeComments(comment));
 
 function initializeComments(comment) {
 	const stampText = comment.querySelector(".stamp").textContent;
